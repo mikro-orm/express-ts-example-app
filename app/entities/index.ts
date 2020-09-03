e875544b9f6d1ba822c37be82ab0d90564f8e0e3
Martin Adámek <banan23@gmail.com>
2020-09-03 09:15:40 +0200

    upgrade to MikroORM 4 rc.6

--- app/entities/index.ts
+++ app/entities/index.ts
@@ -2,3 +2,4 @@ export * from './Author';
 export * from './Book';
 export * from './BookTag';
 export * from './Publisher';
+export * from './BaseEntity';
--- app/mikro-orm.config.ts
+++ app/mikro-orm.config.ts
@@ -1,12 +1,10 @@
 import { Options } from '@mikro-orm/core';
-import { Author, Book, BookTag, Publisher } from './entities';
-import { BaseEntity } from './entities/BaseEntity';
 import { MongoHighlighter } from '@mikro-orm/mongo-highlighter';
+import { Author, Book, BookTag, Publisher, BaseEntity } from './entities';
 
 const options: Options = {
   type: 'mongo',
   entities: [Author, Book, BookTag, Publisher, BaseEntity],
-  entitiesTs: ['app/entities'],
   dbName: 'mikro-orm-express-ts',
   highlighter: new MongoHighlighter(),
   debug: true,
